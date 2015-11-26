c3d044d70a8d36188dd88f04f0be655475967871
Yamashita, Yuu <peek824545201@gmail.com>
2015-11-26 12:39:15 +0000

    Only use premitives in which hooks to avoid infinite loop (#104)

--- etc/pyenv.d/which/python-config.bash
+++ etc/pyenv.d/which/python-config.bash
@@ -4,11 +4,28 @@
 # https://github.com/yyuu/pyenv/issues/397
 
 if [ ! -x "${PYENV_COMMAND_PATH}" ] && [[ "${PYENV_COMMAND_PATH##*/}" == "python"*"-config" ]]; then
-  virtualenv_prefix="$(pyenv-virtualenv-prefix 2>/dev/null || true)"
-  if [ -d "${virtualenv_prefix}" ]; then
-    virtualenv_command_path="${virtualenv_prefix}/bin/${PYENV_COMMAND_PATH##*/}"
-    if [ -x "${virtualenv_command_path}" ]; then
-      PYENV_COMMAND_PATH="${virtualenv_command_path}"
+  OLDIFS="${IFS}"
+  IFS=:
+  version="$(pyenv-version-name)"
+  IFS="${OLDIFS}"
+  if [ -f "${PYENV_ROOT}/versions/${version}/bin/activate" ]; then
+    if [ -f "${PYENV_ROOT}/versions/${version}/bin/conda" ]; then
+      : # do nothing for conda's environments
+    else
+      if [ -f "${PYENV_ROOT}/versions/${version}/bin/pyvenv.cfg" ]; then
+        # pyvenv
+        virtualenv_binpath="$(cut -b 1-1024 "${PYENV_ROOT}/versions/${version}/pyvenv.cfg" | sed -n '/^ *home *= */s///p' || true)"
+        virtualenv_prefix="${virtualenv_binpath%/bin}"
+      else
+        # virtualenv
+        shopt -s nullglob
+        virtualenv_prefix="$(cat "${PYENV_ROOT}/versions/${version}/lib/"*"/orig-prefix.txt" </dev/null 2>&1 || true)"
+        shopt -u nullglob
+      fi
+      virtualenv_command_path="${virtualenv_prefix}/bin/${PYENV_COMMAND_PATH##*/}"
+      if [ -x "${virtualenv_command_path}" ]; then
+        PYENV_COMMAND_PATH="${virtualenv_command_path}"
+      fi
     fi
   fi
 fi

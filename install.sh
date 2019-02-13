e39b1126767b2244480fb47c0b78374f5c671eba
Yamashita, Yuu <peek824545201@gmail.com>
2019-02-13 15:24:30 +0000

    Install libexec as well under the given prefix (#290, #302)

--- install.sh
+++ install.sh
@@ -12,14 +12,17 @@ if [ -z "${PREFIX}" ]; then
 fi
 
 BIN_PATH="${PREFIX}/bin"
+LIBEXEC_PATH="${PREFIX}/libexec"
 SHIMS_PATH="${PREFIX}/shims"
 HOOKS_PATH="${PREFIX}/etc/pyenv.d"
 
 mkdir -p "$BIN_PATH"
+mkdir -p "$LIBEXEC_PATH"
 mkdir -p "$SHIMS_PATH"
 mkdir -p "$HOOKS_PATH"
 
 install -p bin/* "$BIN_PATH"
+install -p libexec/* "$LIBEXEC_PATH"
 install -p shims/* "$SHIMS_PATH"
 for hook in etc/pyenv.d/*; do
   if [ -d "$hook" ]; then
